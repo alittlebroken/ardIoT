dfface35c620cb409dd48105a306d446874eb8c1
Paul Lockyer <plockyer@googlemail.com>
2019-11-04 16:19:26 +0000

    Create configuration.php

---
+++ core/configuration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The applications configuration file
+ * 
+ * @author Paul Lockyer <plockyer@googlemail.com>
+ *
+ */
+ 
+ /**
+  * Database defines
+  *
+  */
+  define("DBHOST","localhost");
+  define("DBNAME","ardiot");
+  define("DBPORT","3306");
+  define("DBCHARSET","utf8mb4");
+  define("DBUID","ardiot_admin");
+  define("DBPASS","ardiot_admin");    // Goes without saying but change this when deploying in production
+  
